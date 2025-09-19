22294909e3211b147b1a13ae4428958c788e03c8
Kimi0903 <kimberlpena@gmail.com>
2025-09-19 16:42:17 -0600

    Update sw.js

--- Actividatfetchysw/sw.js
+++ Actividatfetchysw/sw.js
@@ -1,17 +1,18 @@
 self.addEventListener(
   'fetch',
-  function(event){
-    console.log(event.request.url);
-    if(/\.jpg$/.test(evento.request.url)){
-      event.respondWith(
+  function(evento){
+    console.log(evento.request.url);
+    if(/\.jpg$/.test(evento.request.url)) {
+      evento.respondWith(
         fetch('unicorn.png')
       );
     }
-    else if(/\.png$/.test(evento.request.url)){
+    else if(/\.png$/.test(evento.request.url)) {
       evento.respondWith(
         fetch('Gato.jpg')
       );
     }
   }
-)
+);
+
 
