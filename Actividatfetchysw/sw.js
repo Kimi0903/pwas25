206bc9e77cfde647992be3f47c6ea945ef15f71d
Kimi0903 <kimberlpena@gmail.com>
2025-09-26 16:50:29 -0600

    Update sw.js

--- Actividatfetchysw/sw.js
+++ Actividatfetchysw/sw.js
@@ -1,18 +1,52 @@
+var cacheName = 'cache1';
+
 self.addEventListener(
-  'fetch',
+  'install',
   function(evento){
-    console.log(evento.request.url);
-    if(/\.jpg$/.test(evento.request.url)) {
-      evento.respondWith(
-        fetch('unicorn.png')
-      );
-    }
-    else if(/\.png$/.test(evento.request.url)) {
-      evento.respondWith(
-        fetch('Gato.jpg')
-      );
-    }
+    evento.waitUntil(
+      caches.open(nombreCache)
+      .then(
+        function(cache){
+          cache.addA11([
+            'index.html',
+            'bootstrap-5.3.8-dist/css/bootstrap.min.css',
+            'bootstrap-5.3.8-dist/js/bootstrap.min.js',
+            'lib1.js',
+            'lib2.js',
+            'unicorn.jpg',
+
+          ]
+          );
+        }
+      )
+    );
   }
 );
 
+self.addEventListener(
+    'fetch',
+    function (evento) {
+        if (/\.jpg$/.test(evento.request.url)) {
+            evento.respondWith(fetch('unicorn.jpg'));
+        }
+        else if (/\.png$/.test(evento.request.url)) {
+            evento.respondWith(
+                fetch('unicorn.png')
+            );
+        }
+        else {
+            evento.respondWith(
+                caches.match(evento.request)
+                    .then(
+                        function (respuesta) {
+                            if (respuesta) {
+                                return respuesta;
+                            }
+                            return fetch(evento.request);
+                        }
+                    )
+            );
+        }
+    }
+);
 
